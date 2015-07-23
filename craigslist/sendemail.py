97c47185a54fbf05b91babbfbe73c28091b45804
HuaiZhong Liu <lhuaizhong@gmail.com>
2015-07-23 21:33:29 +0800

    Email sender is built.

--- craigslist/sendemail.py
+++ craigslist/sendemail.py
@@ -3,17 +3,19 @@ import sys
 import smtplib
 from email.mime.text import MIMEText
 import ConfigParser
-
+import datetime
+import time
 import logging
+import csv
 
-def send_mail(cfgfile, messages):
-    logging.info('Send alerts email.')
+def send_mail(cfgfile, email, messages):
+    logging.info('Send email to %s.' % email)
     #mailto_list=["aaa@your.com"]
     #mail_host="smtp.gmail.com:587"
     #mail_user="youraccount"
     #mail_pass="yourpassword"
     #mail_postfix="gmail.com"
-    sub = 'Error alerts from get_reports program!'
+    sub = 'Say hello to you from program!'
     config = ConfigParser.RawConfigParser()
     config.read(cfgfile)
     mail_host = config.get('mail','mail_host')
@@ -21,11 +23,12 @@ def send_mail(cfgfile, messages):
     mail_pass = config.get('mail','mail_pass')
     mail_postfix = config.get('mail','mail_postfix')
     #mail_from = config.get('mail','mail_from')
-    to_list = []
-    to_list.append(config.get('mail','mailto_list'))
+    to_list = [email,]
+    #to_list.append(config.get('mail','mailto_list'))
     #print mailto_list
 
-    content = os.linesep.join(messages)
+    #content = os.linesep.join(messages)
+    content = messages
     me=mail_user+"<"+mail_user+"@"+mail_postfix+">"
     msg = MIMEText(content)
     msg['Subject'] = sub
@@ -52,7 +55,27 @@ def main():
     start_time = time.clock()
     logging.basicConfig(filename='logging.txt', format="%(asctime)s;%(levelname)s;%(message)s", level=logging.DEBUG)
     logging.info('Start to send emails...')
-    send_mail('sendemail.ini', 'test')
+    fin = open('new.csv')
+    fout = open('master.csv', 'ab')
+    reader = csv.reader(fin)
+    writer = csv.writer(fout)
+    reader.next()
+    for row in reader:
+        #print row
+        content = 'Missing content file.'
+        with open('email-content.txt') as f:
+            content = f.read()
+        email = row[-2]
+        if email:
+            ###
+            #r = send_mail('sendemail-lhz.ini', email, content)
+            r = send_mail('sendemail.ini', email, content)
+            if r:
+                row[-1] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        writer.writerow(row)
+
+    fin.close()
+    fout.close()
     print 'Completed in', time.clock() - start_time, 'seconds'
 
 if __name__ == '__main__':
